6f585ce80a72a33b12caf980b2581b5d862aab5b
Sublime <rene.knap.92ooglemail.com>
2024-08-01 06:30:01 +0200

    Adding button and slider to control animation

--- Sound-Pressure-Simulation.py
+++ Sound-Pressure-Simulation.py
@@ -1,27 +1,31 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
+from matplotlib.widgets import Slider, Button
 
 
 
-posStepSize = 0.1
-timeStepSize = 0.0001
+posStepSize = 0.05
+timeStepSize = 0.00005
 SPEED_OF_SOUND = 346.3  # speed of sound in air in m/s at 25°C
 
-roomWidth = 10.0  # in meters
-roomHeight = 10.0  # in meters
+roomWidth = 5.0  # in meters
+roomHeight = 3.6   # in meters
 
 numDiscretePosX = int(roomWidth / posStepSize) + 1
 numDiscretePosY = int(roomHeight / posStepSize) + 1
 
-pressureField = np.zeros((numDiscretePosX, numDiscretePosY))
-velocityFieldX = np.zeros((numDiscretePosX, numDiscretePosY))
-velocityFieldY = np.zeros((numDiscretePosX, numDiscretePosY))
+pressureField = np.zeros((numDiscretePosY, numDiscretePosX))
+velocityFieldX = np.zeros((numDiscretePosY, numDiscretePosX))
+velocityFieldY = np.zeros((numDiscretePosY, numDiscretePosX))
 
-sourcePosX = numDiscretePosX // 2
-sourcePosY = numDiscretePosY // 2
+sourcePosX = numDiscretePosY // 2
+sourcePosY = numDiscretePosX // 2
 frequency = 100  # in Hz
 omega = 2 * np.pi * frequency
+
+
+animRunning = True
 
 
 fig, ax = plt.subplots()
@@ -42,6 +46,17 @@ ax.set_yticks(yTicks)
 ax.set_yticklabels(yLabels)
 
 
+axFrequency = plt.axes([0.2, 0.01, 0.4, 0.03], facecolor='lightgoldenrodyellow')
+sliderFrequency = Slider(axFrequency, 'Frequency', 10, 1000, valinit=frequency)
+
+axReset = plt.axes([0.72, 0.01, 0.1, 0.04])
+buttonReset = Button(axReset, 'Reset', color='lightgoldenrodyellow', hovercolor='0.975')
+
+axStop = plt.axes([0.88, 0.01, 0.1, 0.04])
+buttonStop = Button(axStop, 'Stop', color='lightgoldenrodyellow', hovercolor='0.975')
+
+
+
 def calcFiniteDifferenceTimeDomain(pressureField, velocityFieldX, velocityFieldY, frame):
     # update velocity fields
     velocityFieldX[1:, :] -= timeStepSize / posStepSize * (pressureField[1:, :] - pressureField[:-1, :])
@@ -53,27 +68,46 @@ def calcFiniteDifferenceTimeDomain(pressureField, velocityFieldX, velocityFieldY
     velocityFieldY[:, 0] = 0
     velocityFieldY[:, -1] = 0
 
-
     pressureField[:-1, :] -= timeStepSize * SPEED_OF_SOUND**2 / posStepSize * (velocityFieldX[1:, :] - velocityFieldX[:-1, :])
     pressureField[:, :-1] -= timeStepSize * SPEED_OF_SOUND**2 / posStepSize * (velocityFieldY[:, 1:] - velocityFieldY[:, :-1])
 
-
     # simple sound source
     pressureField[sourcePosX, sourcePosY] += np.sin(omega * frame * timeStepSize)
-
-
-
     return pressureField, velocityFieldX, velocityFieldY
 
 def update(frame):
-    global pressureField, velocityFieldX, velocityFieldY
+    global pressureField, velocityFieldX, velocityFieldY, animRunning
 
-    pressureField, velocityFieldX, velocityFieldY = calcFiniteDifferenceTimeDomain(
-        pressureField, velocityFieldX, velocityFieldY, frame)
+    if animRunning:
+        pressureField, velocityFieldX, velocityFieldY = calcFiniteDifferenceTimeDomain(
+            pressureField, velocityFieldX, velocityFieldY, frame)
 
-    image.set_array(pressureField[:-1, :-1])
+        image.set_array(pressureField[:-1, :-1])
     return [image]
 
+def update_frequency(val):
+    global frequency, omega
+    frequency = sliderFrequency.val
+    omega = 2 * np.pi * frequency
 
-animation = FuncAnimation(fig, update, blit=True, interval=20, cache_frame_data=False)
+def reset(event):
+    global pressureField, velocityFieldX, velocityFieldY
+    pressureField = np.zeros((numDiscretePosY, numDiscretePosX))
+    velocityFieldX = np.zeros((numDiscretePosY, numDiscretePosX))
+    velocityFieldY = np.zeros((numDiscretePosY, numDiscretePosX))
+
+def stop(event):
+    global animRunning
+    animRunning = not animRunning
+    if animRunning:
+        buttonStop.label.set_text('Stop')
+    else:
+        buttonStop.label.set_text('Start')
+
+sliderFrequency.on_changed(update_frequency)
+buttonReset.on_clicked(reset)
+buttonStop.on_clicked(stop)
+
+
+animation = FuncAnimation(fig, update, blit=True, interval=2, cache_frame_data=False)
 plt.show()
