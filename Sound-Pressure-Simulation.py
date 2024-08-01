fb039927307d7f427658b03661e30bc33cd230dd
Sublime <rene.knap.92ooglemail.com>
2024-08-01 04:17:44 +0200

    Adding a Pressure animation by Finite Difference Time Domain calculation with a simple sound source

--- Sound-Pressure-Simulation.py
+++ Sound-Pressure-Simulation.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
-posStepSize = 0.1
+posStepSize = 0.05
 timeStepSize = 0.0001
 SPEED_OF_SOUND = 346.3  # speed of sound in air in m/s at 25°C
 numSteps = 300
@@ -13,18 +13,52 @@ roomHeight = 10.0  # in meters
 numDiscretePosX = int(roomWidth / posStepSize)
 numDiscretePosY = int(roomHeight / posStepSize)
 
-pressure = np.zeros((numDiscretePosX, numDiscretePosY))
+pressureField = np.zeros((numDiscretePosX, numDiscretePosY))
+velocityFieldX = np.zeros((numDiscretePosX, numDiscretePosY))
+velocityFieldY = np.zeros((numDiscretePosX, numDiscretePosY))
+
+sourcePosX = numDiscretePosX // 2
+sourcePosY = numDiscretePosY // 2
+frequency = 100  # in Hz
+omega = 2 * np.pi * frequency
+
 
 fig, ax = plt.subplots()
-image = ax.imshow(pressure, cmap='viridis', vmin=-0.1, vmax=0.1, animated=True)
-ax.set_title('Sound Pressure Simulation')
+image = ax.imshow(pressureField, cmap='viridis', vmin=-0.1, vmax=0.1, animated=True)
+ax.set_title('Sound PressureField Simulation')
 ax.set_xlabel('X in meter')
 ax.set_ylabel('Y in meter')
 
+
+def calcFiniteDifferenceTimeDomain(pressureField, velocityFieldX, velocityFieldY, frame):
+    # update velocity fields
+    velocityFieldX[1:, :] -= timeStepSize / posStepSize * (pressureField[1:, :] - pressureField[:-1, :])
+    velocityFieldY[:, 1:] -= timeStepSize / posStepSize * (pressureField[:, 1:] - pressureField[:, :-1])
+
+    # reflective boundary conditions
+    velocityFieldX[0, :] = 0
+    velocityFieldX[-1, :] = 0
+    velocityFieldY[:, 0] = 0
+    velocityFieldY[:, -1] = 0
+
+    # update pressure field
+    pressureField[:-1, :] -= timeStepSize * SPEED_OF_SOUND**2 / posStepSize * (velocityFieldX[1:, :] - velocityFieldX[:-1, :])
+    pressureField[:, :-1] -= timeStepSize * SPEED_OF_SOUND**2 / posStepSize * (velocityFieldY[:, 1:] - velocityFieldY[:, :-1])
+
+    # simple sound source
+    pressureField[sourcePosX, sourcePosY] += np.sin(omega * frame * timeStepSize)
+
+    return pressureField, velocityFieldX, velocityFieldY
+
 def update(frame):
-	global pressure
-	image.set_array(pressure)
-	return [image]
+    global pressureField, velocityFieldX, velocityFieldY
+
+    pressureField, velocityFieldX, velocityFieldY = calcFiniteDifferenceTimeDomain(
+        pressureField, velocityFieldX, velocityFieldY, frame
+    )
+
+    image.set_array(pressureField)
+    return [image]
 
 
 animation = FuncAnimation(fig, update, frames=numSteps, blit=True, interval=20)
