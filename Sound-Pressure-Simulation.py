9e4d2d7744942c7cbf3c5e2a54c2254d6d5c2032
Sublime <rene.knap.92ooglemail.com>
2024-08-02 06:35:44 +0200

    Adding a speaker size and drawing in the speaker

--- Sound-Pressure-Simulation.py
+++ Sound-Pressure-Simulation.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 from matplotlib.widgets import Slider, Button
-
+from matplotlib.patches import Circle
 
 
 posStepSize = 0.05
@@ -19,8 +19,9 @@ pressureField = np.zeros((numDiscretePosY, numDiscretePosX))
 velocityFieldX = np.zeros((numDiscretePosY, numDiscretePosX))
 velocityFieldY = np.zeros((numDiscretePosY, numDiscretePosX))
 
-sourcePosX = numDiscretePosY // 4
-sourcePosY = numDiscretePosX // 2
+speakerSize = 0.3  # in meters
+speakerPosX = numDiscretePosY // 4
+speakerPosY = numDiscretePosX // 2
 frequency = 500  # in Hz
 omega = 2 * np.pi * frequency
 
@@ -34,7 +35,6 @@ ax.set_title('Sound Pressure Simulation')
 ax.set_xlabel('X in meter')
 ax.set_ylabel('Y in meter')
 
-# Set ticks and labels for x and y axes
 xTicks = np.arange(0, numDiscretePosX, int(1 / posStepSize))
 xLabels = np.round(xTicks * posStepSize, 1)
 ax.set_xticks(xTicks)
@@ -45,6 +45,9 @@ yLabels = np.round(yTicks * posStepSize, 1)
 ax.set_yticks(yTicks)
 ax.set_yticklabels(yLabels)
 
+speakerCircle = Circle(
+    (speakerPosY, speakerPosX), radius=speakerSize / posStepSize / 2, color='orange', fill=False, linewidth=2)
+ax.add_patch(speakerCircle)
 
 axFrequency = plt.axes([0.2, 0.01, 0.4, 0.03], facecolor='lightgoldenrodyellow')
 sliderFrequency = Slider(axFrequency, 'Frequency', 10, 1000, valinit=frequency)
@@ -56,8 +59,7 @@ axStop = plt.axes([0.88, 0.01, 0.1, 0.04])
 buttonStop = Button(axStop, 'Stop', color='lightgoldenrodyellow', hovercolor='0.975')
 
 
-
-def calcFiniteDifferenceTimeDomain(pressureField, velocityFieldX, velocityFieldY, currentPhase):
+def calcFiniteDifferenceTimeDomain(pressureField, velocityFieldX, velocityFieldY, speakerSize, currentPhase):
     # update velocity fields
     velocityFieldX[1:, :] -= timeStepSize / posStepSize * (pressureField[1:, :] - pressureField[:-1, :])
     velocityFieldY[:, 1:] -= timeStepSize / posStepSize * (pressureField[:, 1:] - pressureField[:, :-1])
@@ -72,33 +74,45 @@ def calcFiniteDifferenceTimeDomain(pressureField, velocityFieldX, velocityFieldY
     pressureField[:, :-1] -= timeStepSize * SPEED_OF_SOUND**2 / posStepSize * (velocityFieldY[:, 1:] - velocityFieldY[:, :-1])
 
     # simple sound source
-    pressureField[sourcePosX, sourcePosY] += np.sin(currentPhase)
+    updateSpeakerPressure(pressureField, speakerPosX, speakerPosY, speakerSize, currentPhase)
     currentPhase += omega * timeStepSize
     currentPhase %= 2 * np.pi
 
     return pressureField, velocityFieldX, velocityFieldY, currentPhase
 
-def update(frame):
-    global pressureField, velocityFieldX, velocityFieldY, animRunning, currentPhase
+def updateSpeakerPressure(pressureField, centerX, centerY, size, phase):
+    radius = int(size / posStepSize / 2)
+    for y in range(centerX - radius, centerX + radius):
+        for x in range(centerY - radius, centerY + radius):
+            if 0 <= x < numDiscretePosX and 0 <= y < numDiscretePosY:
+                if (x - centerY)**2 + (y - centerX)**2 <= radius**2:
+                    pressureField[y, x] += np.sin(phase)
 
+def update(frame):
+    global pressureField, velocityFieldX, velocityFieldY, animRunning, speakerSize, currentPhase
 
     if animRunning:
         pressureField, velocityFieldX, velocityFieldY, currentPhase = calcFiniteDifferenceTimeDomain(
-            pressureField, velocityFieldX, velocityFieldY, currentPhase)
+            pressureField, velocityFieldX, velocityFieldY, speakerSize, currentPhase)
 
         image.set_array(pressureField[:-1, :-1])
-    return [image]
 
-def update_frequency(val):
+        # draw Speaker
+        speakerCircle.set_radius(speakerSize / posStepSize / 2)
+        ax.add_patch(speakerCircle)
+    return [image, speakerCircle]
+
+def updateFrequency(val):
     global frequency, omega
     frequency = sliderFrequency.val
     omega = 2 * np.pi * frequency
 
 def reset(event):
-    global pressureField, velocityFieldX, velocityFieldY
+    global pressureField, velocityFieldX, velocityFieldY, currentPhase
     pressureField = np.zeros((numDiscretePosY, numDiscretePosX))
     velocityFieldX = np.zeros((numDiscretePosY, numDiscretePosX))
     velocityFieldY = np.zeros((numDiscretePosY, numDiscretePosX))
+    currentPhase = 0
 
 def stop(event):
     global animRunning
@@ -108,10 +122,9 @@ def stop(event):
     else:
         buttonStop.label.set_text('Start')
 
-sliderFrequency.on_changed(update_frequency)
+sliderFrequency.on_changed(updateFrequency)
 buttonReset.on_clicked(reset)
 buttonStop.on_clicked(stop)
-
 
 animation = FuncAnimation(fig, update, blit=True, interval=2, cache_frame_data=False)
 plt.show()
